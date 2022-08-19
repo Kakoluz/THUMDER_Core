a07a8376f478a1617f10bc7f35ace8fc002508c0
Alberto Rodríguez Torres <alberto_kakolukylla@hotmail.com>
2022-08-19 11:54:36 +0200

    Added emulation constants and memory to avoid changes to assembler down the road.

---
+++ THUMDER Core/Deluxe/Memory.cs
@@ -0,0 +1,56 @@
+﻿namespace THUMDER.Deluxe
+{
+    internal class Memory
+    {
+        /// <summary>
+        /// Memory as a 32-bit unsigned integer array. 
+        /// </summary>
+        private uint[] memory;
+
+        /// <summary>
+        /// Singleton instance internal value
+        /// </summary>
+        private static Memory? instance = null;
+        /// <summary>
+        /// Memory singleton instance
+        /// </summary>
+        private static Memory Instance
+        {
+            set => instance = value;
+            get
+            {   //Might need to add locks or lazy implementation for thread safety.
+                if (instance == null)
+                    instance = new Memory();
+                return instance;
+            }
+        }
+        private Memory() //The Memory constructor should only be called once per execution  
+        {
+            this.memory = new uint[32768];
+        }
+
+        /// <summary>
+        /// Unified memory access to emulate the hardware behaviour.
+        /// </summary>
+        /// <param name="Address"> Emulated address bus.</param>
+        /// <param name="Data"> Emulated data bus.</param>
+        /// <param name="RW"> Specifies mode, high for writes, low for reads.</param>
+        public static void MemoryAccess(in uint Address, ref byte[] Data, in bool RW)
+        {
+            if (!RW)
+                Data = BitConverter.GetBytes(Instance.memory[Address]);
+            else
+                Instance.memory[Address] = BitConverter.ToUInt32(Data);
+            return; 
+        }
+
+        /// <summary>
+        /// Clears the memory and resizes it to the specified value.
+        /// </summary>
+        /// <param name="newSize">New memory size.</param>
+        public static void ResizeMemory(in uint newSize)
+        {
+            Instance.memory = new uint[newSize];
+        }
+    }
+}
---
+++ THUMDER Core/Deluxe/SimManager.cs
@@ -0,0 +1,66 @@
+﻿namespace THUMDER.Deluxe
+{
+    internal sealed class SimManager
+    {
+
+        //Set C# auto properties for emulation settings and initialize to WinDLX default values
+        /// <summary>
+        /// Data forwarding status.
+        /// </summary>
+        public static bool Forwarding { get; set; } = true;
+
+        /// <summary>
+        /// Size of the memory.
+        /// </summary>
+        public static int Memsize { get; set; } = 32768;
+
+        /// <summary>
+        /// Number of summator units for floating point.
+        /// </summary>
+        public static int ADDUnits { get; set; } = 1;
+        /// <summary>
+        /// Number of multiplier units for floating point.
+        /// </summary>
+        public static int MULUnits { get; set; } = 1;
+        /// <summary>
+        /// Number of divisor units for floating point.
+        /// </summary>
+        public static int DIVUnits { get; set; } = 1;
+        
+        /// <summary>
+        /// Number of cicles for a floating point add operation.
+        /// </summary>
+        public static int ADDDelay  { get; set; } = 2;
+        /// <summary>
+        /// Number of cicles for a floating point multiplication operation.
+        /// </summary>
+        public static int MULDDelay { get; set; } = 5;
+        /// <summary>
+        /// Number of cicles for a floatig point division operation.
+        /// </summary>
+        public static int DIVDelay { get; set; } = 19;
+
+        /// <summary>
+        /// Singleton internal instance.
+        /// </summary>
+        private static SimManager? instance = null;
+        /// <summary>
+        /// Singleton instance.
+        /// </summary>
+        public static SimManager Instance
+        {
+            private set => instance = value;
+            get
+            {   //Might need to add locks or lazy implementation for thread safety.
+                if (instance == null)
+                    instance = new SimManager();
+                return instance;
+            }
+        }
+
+        private SimManager()
+        {
+            
+        }
+    }
+}
